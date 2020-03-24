fe10c420ef29127bca56916fd91f589672e107c6
Olegasm <oleg.mirzianov@gmail.com>
2020-03-25 01:29:08 +0200

    Update __init__.py

--- example/__init__.py
+++ example/__init__.py
@@ -3,10 +3,19 @@ import check50.c
 
 @check50.check()
 def exists():
-    """hello.c exists"""
+    """hello.c egzistuoja"""
     check50.exists("U1.c")
     
 @check50.check(exists)
 def compiles():
-    """U1.c compiles."""
+    """U1.c kompiliuojasi."""
     check50.c.compile("U1.c", lcs50=True)
+    
+@check50.check(compiles)
+def test1():
+    """Atspausdina korektišką rezultatą"""
+    check_file(open("U1rez.txt").read(), open("8.txt").read())
+
+def check_file(output, correct):
+    if output == correct:
+        return
