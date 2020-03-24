5f0a9d996797309e8c410447f45447ded8a112c9
Olegasm <oleg.mirzianov@gmail.com>
2020-03-25 00:47:08 +0200

    Update __init__.py

--- example/__init__.py
+++ example/__init__.py
@@ -1,6 +1,12 @@
 import check50
+import check50
 
 @check50.check()
 def exists():
-    """hello.py exists"""
+    """hello.c exists"""
     check50.exists("U1.c")
+    
+@check50.check(exists)
+def compiles():
+    """U1.c compiles."""
+    check50.c.compile("U1o.c", lcs50=True)
