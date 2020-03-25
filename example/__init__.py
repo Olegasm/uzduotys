53af18fbce1b7a1016381088e748634e7eb54718
Olegasm <oleg.mirzianov@gmail.com>
2020-03-25 21:20:36 +0200

    Update __init__.py

--- example/__init__.py
+++ example/__init__.py
@@ -26,3 +26,15 @@ def test1():
 def check_file(output, correct):
     if output == correct:
         return
+    
+    output = [line for line in output.splitlines() if line != ""]
+    correct = correct.splitlines()
+
+    help = None
+    if len(output) == len(correct):
+        if all(ol.rstrip() == cl for ol, cl in zip(output, correct)):
+            help = "did you add too much trailing whitespace to the end of your pyramid?"
+        elif all(ol[1:] == cl for ol, cl in zip(output, correct)):
+            help = "are you printing an additional character at the beginning of each line?"
+
+    raise check50.Mismatch(correct, output, help=help)    
