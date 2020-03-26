64f04078793693ccdd45761e3e0c02ee0cd58478
Olegasm <oleg.mirzianov@gmail.com>
2020-03-26 14:27:24 +0200

    Update __init__.py

--- example/__init__.py
+++ example/__init__.py
@@ -46,7 +46,7 @@ def compare_files(output, correct):
         return 
     raise check50.Mismatch(correct, output, help= None)
 
-def read_first_file_line(exists)
+def read_first_file_line(exists):
     """Ar teisingai pripilti indai ir kiek liko aliejaus"""
     compare_files(open("U1rez.txt").readline(), open("1.txt").readline())
     
