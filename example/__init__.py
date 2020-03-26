909589e97eb511eec388234c814e359e7a80c2a5
Olegasm <oleg.mirzianov@gmail.com>
2020-03-26 07:38:17 +0200

    Update __init__.py

--- example/__init__.py
+++ example/__init__.py
@@ -7,12 +7,13 @@ def exists():
     """hello.c egzistuoja"""
     check50.exists("U1.c")
     check50.include("1.txt")
-    
+'''    
 @check50.check(exists)
 def compiles():
     """U1.c kompiliuojasi"""
     check50.c.compile("U1.c", lcs50=True)
-    
+'''
+
 @check50.check(exists)
 def compiles():
     """hellp.cc kompiliuojasi"""
